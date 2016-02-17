fd47b2d8cb696e8aaacc97be7fab62c1b1395a06
stefanh <sh@dasistweb.de>
2016-02-17 19:44:00 +0100

    removed unused config variables

--- config.php
+++ config.php
@@ -5,22 +5,16 @@ $config = array(
     # disable the cache
     "nocache" => true,
 
-    # what type of generator should be used by default
-    "default_template_state" => "dynamic",
-
     # our template file extension
     "extension" => "crml",
-
-    # what characters should be used to indent the output files
-    "output_indent" => "    ",
 
     # directory where we want the cache to be located at
     "cache_dir" => "cache",
 
-    # display comment in the frontend
+    # display comments in the frontend
     "show_comments" => true,
 
-    # display comment in the frontend
+    # display block name comments in the frontend
     "show_blocks" => false,
 
     # default self closing items
@@ -32,5 +26,5 @@ $config = array(
 
     # the file header which gets added to all templates
     # if you don't want one, set it to false
-    "file_header" => "Caramel Example Template File &copy; hvlmnns"
+    "file_header" => "Caramel template engine."
 );
