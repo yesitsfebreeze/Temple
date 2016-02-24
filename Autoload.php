13bd34e7f128c55e312b445dc35905a513a07c41
stefanh <sh@dasistweb.de>
2016-02-24 13:07:56 +0100

    removed trailing line

---
+++ Autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+
+# Models
+require_once "Core/Models/Storage.php";
+require_once "Core/Models/Vars.php";
+require_once "Core/Models/Node.php";
+require_once "Core/Models/Plugin.php";
+
+# Services
+require_once "Core/Services/Error.php";
+require_once "Core/Services/Helpers.php";
+require_once "Core/Services/Directories.php";
+require_once "Core/Services/Config.php";
+require_once "Core/Services/Cache.php";
+require_once "Core/Services/Plugins.php";
+require_once "Core/Services/Template.php";
+require_once "Core/Services/Lexer.php";
+require_once "Core/Services/Parser.php";
+
+# Class
+require_once "Caramel.php";
