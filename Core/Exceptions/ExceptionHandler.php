d247373225f4e5546a1f46442dd5a11b65e9b254
shoevelmanns <sh@dasistweb.de>
2016-03-17 23:55:26 +0100

    comment the ExceptionHandler

--- Core/Exceptions/ExceptionHandler.php
+++ Core/Exceptions/ExceptionHandler.php
@@ -2,19 +2,30 @@
 
 namespace Caramel\Exceptions;
 
-
+/**
+ * Class ExceptionHandler
+ * @package Caramel\Exceptions
+ */
 class ExceptionHandler
 {
 
+    /**
+     * adds a global exception handler for caramel exceptions
+     *
+     * ExceptionHandler constructor.
+     */
     public function __construct()
     {
 
+        /** @var $originalHandler */
         $originalHandler = set_exception_handler(NULL);
 
         set_exception_handler(function (\Exception $Exception) use (&$originalHandler) {
             if ($Exception instanceof CaramelException) {
 
+                # include the error template
                 include_once "Error/Error.php";
+
                 return;
             } elseif (is_callable($originalHandler)) {
                 return call_user_func_array($originalHandler, [$Exception]);
