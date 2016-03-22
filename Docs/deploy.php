6aade93d685aff5ba7bd863d81dca83cd12bffef
stefanh <sh@dasistweb.de>
2016-03-22 01:12:40 +0100

    adding yaml

--- Docs/deploy.php
+++ Docs/deploy.php
@@ -1,11 +1,13 @@
 <?php
 
+
 require_once "../vendor/autoload.php";
 
 $tree = new \Docs\MarkDownTree();
 $tree = $tree->getTree();
 
 $parsedown = new Parsedown();
+$yaml      = new \Symfony\Component\Yaml\Yaml();
 
 new \Docs\ParseMarkdown($tree, __DIR__, $parsedown);
-new \Docs\parseTwig(__DIR__);
+new \Docs\parseTwig(__DIR__, $yaml);
--- Docs/parseTwig.php
+++ Docs/parseTwig.php
@@ -3,14 +3,22 @@
 namespace Docs;
 
 
+
+
+use Symfony\Component\Yaml\Yaml;
+
 class parseTwig
 {
-    public function __construct($dir)
+    public function __construct($dir, Yaml $yaml)
     {
+        $config = $dir . "/../config.yml";
+        $config = $yaml->parse(file_get_contents($config));
+        var_dump($config);
+        die();
         $dir = $dir . '/../templates';
         \Twig_Autoloader::register();
-        $loader     = new \Twig_Loader_Filesystem($dir);
-        $twig       = new \Twig_Environment($loader, array('cache' => $dir . "/cache",));
+        $loader = new \Twig_Loader_Filesystem($dir);
+        $twig   = new \Twig_Environment($loader, array('cache' => $dir . "/cache",));
         $twig->clearCacheFiles();
         $full       = $twig->render('index.twig');
         $outputFile = $dir . "/../index.html";
