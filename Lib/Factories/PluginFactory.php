a0ee02cdd5a14cd8574ddf2fb2740fe427e99514
stefanh <sh@dasistweb.de>
2016-06-01 23:56:21 +0200

    working on lever Refactor

---
+++ Lib/Factories/PluginFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Temple\Factories;
+
+
+use Temple\Nodes\BaseNode;
+use Temple\Repositories\StorageRepository;
+use Temple\Services\PluginInitService;
+
+/**
+ * Class configFactory
+ *
+ * @package Contentmanager\Services
+ */
+class PluginFactory extends Factory
+{
+
+    /** @var  PluginInitService $plugins */
+    private $plugins;
+
+
+    /**
+     * @param StorageRepository $plugins
+     */
+    public function setPlugins(StorageRepository $plugins)
+    {
+        $this->plugins = $plugins;
+    }
+
+
+    //    /** @inheritdoc */
+    public function check($class)
+    {
+        $this->getClassName($class);
+        $class = '\\Temple\\Plugins\\' . ucfirst($class) . "Plugin";
+        if (class_exists($class)) {
+            return $class;
+        } else {
+            return NULL;
+        }
+    }
+
+
+    /**
+     * @param BaseNode $node
+     * @return array
+     */
+    public function getPluginsForNode(BaseNode $node)
+    {
+        $plugins = array();
+        $tag     = $node->get("tag.tag");
+        $name    = $node->getName();
+
+        return array();
+    }
+
+}
---
+++ Lib/Models/Dom/Dom.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Temple\Models\Dom;
+
+// todoo: add local variable space
+// todoo: add find method
+use Temple\Utilities\Storage;
+
+/**
+ * Class DomModel
+ *
+ * @package Temple
+ */
+class Dom extends Storage
+{
+
+}
