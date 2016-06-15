53417bca22df4a5d0fbaf8f532f2dbd2560e357e
stefanh <sh@dasistweb.de>
2016-06-15 20:47:26 +0200

    readded Autoload

---
+++ Autoload.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Temple;
+
+
+use Temple\Exception\ExceptionHandler;
+
+$namespace = __NAMESPACE__;
+$dir       = __DIR__ . "/Lib";
+
+spl_autoload_register(function ($class) use ($namespace, $dir) {
+    $class = substr($class, strlen($namespace . "\\"));
+    $file  = $dir . "/" . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file)) {
+        /** @noinspection PhpIncludeInspection */
+        require $file;
+    }
+
+});
+
+new ExceptionHandler();
+
+require_once __DIR__ . "/Instance.php";
