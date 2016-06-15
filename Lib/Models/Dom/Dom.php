cb65431c1eb149ac9f64ef78cede83c931bf367c
stefanh <sh@dasistweb.de>
2016-06-15 20:52:29 +0200

    readded dom

---
+++ Lib/Models/Dom/Dom.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Temple\Models\Dom;
+
+
+// TODO: add local variable space
+// TODO: add find method
+use Temple\Utilities\Storage;
+
+
+/**
+ * Class DomModel
+ *
+ * @package Temple
+ */
+class Dom extends Storage
+{
+
+}
