33be7d525def1e69dde79487e039bfdaba04fcd6
stefanh <sh@dasistweb.de>
2016-07-21 22:11:07 +0200

    update

--- assets.sh
+++ assets.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo chmod -R 0777 *
+
+composer install
+
 # deploy docs
 cp -rf _deployer/_core/api_generator.php _deployer/vendor/evert/phpdoc-md/src/Generator.php
 cd _deployer/vendor/bin/
