19525e3811f0a081d530c6d003237c36b616931e
stefanh <sh@dasistweb.de>
2016-08-31 23:59:29 +0200

    content stuff

--- deploy.php
+++ deploy.php
@@ -1,8 +1,12 @@
 <?php
 
-require_once "Pagen/Pagen/autoload.php";
-require_once "Config.php";
-$config = new \CustomPagenConfig();
-$pagen = new \Pagen\Pagen($config,__DIR__);
-$pagen->deploy();
-echo "page generated...";
+try {
+    require_once "Pagen/Pagen/autoload.php";
+    require_once "Config.php";
+    $config = new \CustomPagenConfig();
+    $pagen  = new \Pagen\Pagen($config, __DIR__);
+    $pagen->deploy();
+    echo "page generated...";
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
